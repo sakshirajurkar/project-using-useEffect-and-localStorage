2f0c8f402ea66311c95347ebab808f0955639baf
sakshi <sakshirajurkar09@gmail.com>
2022-11-30 10:43:18 -0800

    small project added using useEffect and usestate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from "react";
 
 function App() {
+  const [count,setCount]=useState(0);
+  useEffect(()=>{
+    const val = localStorage.getItem("count");
+    if (val){
+      setCount(val);
+    }
+  },[] )
+ 
+ useEffect (()=>{
+  if (count >0){
+    localStorage.setItem("count",count);
+  }
+ },[count])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Counter</h1>
+    <h2>Count:{count}</h2>
+    <button onClick={()=>{setCount(count+1)}}>Increase Count</button>
+    </>
   );
 }
 
